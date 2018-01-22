afff5c3a0ab3adfb0efcd6156633bd46106a2a2c
jvmaia <joaovmferreira@gmail.com>
2018-01-21 22:24:55 -0200

    add host/port in services.py

--- pysy/services.py
+++ pysy/services.py
@@ -2,6 +2,8 @@ from com.android.volley import Request
 from com.android.volley import Response
 from com.android.volley import toolbox
 from com.android.volley.toolbox import Volley
+from .server import host, port
+print(host, port)
 
 class api():
     def __init__(self, activity):
